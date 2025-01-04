08198a0ff17e94261809291ec706213c051e1178
har-sat <harsat.ponnusamy@gmail.com>
2025-01-04 11:36:23 +0530

    chnaged nav to NavLink

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,16 +1,31 @@
-import { Link } from "react-router-dom";
-import classes from "./MainNavigation.module.css"
+import { NavLink } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
 
 export default function MainNavigation() {
   return (
-    <header className= {classes.header}>
+    <header className={classes.header}>
       <nav>
-        <ul className= {classes.list}>
+        <ul className={classes.list}>
           <li>
-            <Link to={"/"}>Home</Link>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to={"/products"}>Products</Link>
+            <NavLink
+              to={"/products"}
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>

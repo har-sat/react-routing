35144e99d1a487a3dcc999d0497872c5117cfc1b
har-sat <harsat.ponnusamy@gmail.com>
2025-01-04 10:52:03 +0530

    router initial setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { createBrowserRouter,Route,RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+
+
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <h1>Your are at Home!</h1>;
+}

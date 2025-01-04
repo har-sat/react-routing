f64bcdbb3d67e2fa5dcb8e15810117730a6740fe
har-sat <harsat.ponnusamy@gmail.com>
2025-01-04 11:29:24 +0530

    added ErrorPage fallback

---
+++ src/pages/Error.js
@@ -0,0 +1,9 @@
+import MainNavigation from "../components/MainNavigation";
+
+export default function ErrorPage() {
+  return <>
+    <MainNavigation />
+    <h1> An Error Occured</h1>
+    <p> Couldn't find this page, may theres a typo? </p>
+  </>
+};

ce12b26464195bcd8970fa0342eb6f86b2c56568
har-sat <harsat.ponnusamy@gmail.com>
2025-01-04 21:14:12 +0530

    feat : loader added to events

--- react-router-project/frontend/src/App.js
+++ react-router-project/frontend/src/App.js
@@ -1,11 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import RootLayoutPage from "./pages/Root";
+import EventsRootLayout from "./pages/EventsRoot";
 import HomePage from "./pages/Home";
 import EventsPage from "./pages/Events";
 import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
-import RootLayoutPage from "./pages/Root";
-import EventsRootLayout from "./pages/EventsRoot";
+
+import { loader as eventsLoader } from "./pages/Events";
 
 const router = createBrowserRouter([
   {
@@ -17,7 +20,11 @@ const router = createBrowserRouter([
         path: "events",
         element: <EventsRootLayout />,
         children: [
-          { index: true, element: <EventsPage /> },
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
           { path: ":id", element: <EventDetailPage /> },
           { path: "new", element: <NewEventPage /> },
           { path: ":id/edit", element: <EditEventPage /> },
